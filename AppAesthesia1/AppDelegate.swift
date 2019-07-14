1f92a51e565e961ee87589d2dd8f4ac12d6d05f7
Christian Popp <christian.popp@gmx.at>
2019-07-14 09:52:17 +0200

    Firebase und Login View Controller initialisiert

--- AppAesthesia1/AppDelegate.swift
+++ AppAesthesia1/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FIRApp.configure()
+        
+        let myDatabase = FIRDatabase.database().reference()
+        myDatabase.setValue("We've got Data!")
+        
         return true
     }
 
