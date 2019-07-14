e7677e936106c0db3372731d708453edf7071c56
Christian Popp <christian.popp@gmx.at>
2019-07-14 11:59:24 +0200

    Register View Controller

---
+++ AppAesthesia1/Controller/RegisterViewController.swift
@@ -0,0 +1,38 @@
+//
+//  RegisterViewController.swift
+//  AppAesthesia1
+//
+//  Created by Christian Popp on 14.07.19.
+//  Copyright © 2019 Christian Popp. All rights reserved.
+//
+
+import UIKit
+import ChameleonFramework
+import Firebase
+
+
+class RegisterViewController: UIViewController {
+
+    // Email und Password Outlets
+    
+    @IBOutlet var emailTextfield: UITextField!
+    @IBOutlet var passwordTextfield: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    @IBAction func registerPressed(_ sender: UIButton) {
+    
+    // Neuen User auf Firebas Database erzeugen
+        
+        
+        
+    }
+    
+
+}
